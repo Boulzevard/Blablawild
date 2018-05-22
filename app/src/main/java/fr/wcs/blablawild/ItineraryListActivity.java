cbb762a26f77ab85dc52267521d5a795644e54be
WOLFF Benjamin <wolfbenjamin31@gmail.com>
2018-05-22 11:37:20 +0200

    quete recyclerview

--- app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ItineraryListActivity.java
@@ -1,14 +1,19 @@
 package fr.wcs.blablawild;
 
 import android.content.Intent;
+import android.graphics.Movie;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ItineraryListActivity extends AppCompatActivity {
 
@@ -18,25 +23,36 @@ public class ItineraryListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_itinerary_list);
 
         SearchModel monobjet = getIntent().getExtras().getParcelable("maclef");
-        setTitle(monobjet.getDeparture() + ">>" +monobjet.getDestination() );
+        setTitle(monobjet.getDeparture() + ">>" + monobjet.getDestination() );
 
         Toast.makeText(this, monobjet.getDate(), Toast.LENGTH_SHORT).show();
 
-        ListView listTrip = findViewById(R.id.list_view);
-        ArrayList<TripModel> results = new ArrayList<>();
+        final RecyclerView listItineraries = findViewById(R.id.list_itineraries);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        listItineraries.setLayoutManager(layoutManager);
 
-        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy-hh:mm");
+        final ArrayList<ItineraryModel> itineraryModels = new ArrayList<>();
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Eric Cartman", new Date(), 15));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Stan Marsh", new Date(), 20));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Kenny Broflovski", new Date(), 12));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Kyle McCormick", new Date(), 18));
+        itineraryModels.add(new ItineraryModel("Paris", "Tokyo", "Wendy Testaburger", new Date(), 16));
 
-        try {
-            results.add(new TripModel("Eric", "Cartman", sdf.parse("21/02/2017-15:30"), 15));
-            results.add(new TripModel("Stan", "Marsh", sdf.parse("21/02/2017-16:00"), 20));
-            results.add(new TripModel("Kenny", "Broflovski", sdf.parse("21/02/2017-16:30"), 16));
-            results.add(new TripModel("Kyle", "McCormick", sdf.parse("21/02/2017-17:00"), 40));
-            results.add(new TripModel("Wendy", "Testaburger", sdf.parse("21/02/2017-17:30"), 20));
-        } catch (ParseException e) {
-        }
-        TripAdapter adapter = new TripAdapter(this, results);
-        listTrip.setAdapter(adapter);
+        final ItineraryRecyclerAdapter adapter = new ItineraryRecyclerAdapter(itineraryModels);
+        listItineraries.setAdapter(adapter);
+
+        listItineraries.addOnItemTouchListener(new RecyclerTouchListener(getApplicationContext(), listItineraries, new RecyclerTouchListener.ClickListener() {
+            @Override
+            public void onClick(View view, int position) {
+                ItineraryModel objet = itineraryModels.get(position);
+                Toast.makeText(getApplicationContext(), objet.getDriver() + " is selected!", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onLongClick(View view, int position) {
+
+            }
+        }));
 
     }
 
